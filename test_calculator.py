663a23fd349bca78b602f169ff45abeec246638d
shamsudheent <shamsudheeent@gmail.com>
2025-07-06 17:15:48 +0530

    corrected the code

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,16 @@
 # test_calculator.py
 
+import sys
+import os
 
-import calculator
+import calc
 
 def test_add():
-    assert calculator.add(2, 3) == 5
+    assert calc.add(2, 3) == 5
 
 
 def test_subtract():
-    assert calculator.subtract(5, 2) == 3
+    assert calc.subtract(5, 2) == 3
 
 def test_add():
-    assert calculator.add(7, 8) == 15
+    assert calc.add(7, 8) == 15
